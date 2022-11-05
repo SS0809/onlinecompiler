9e0ecfab97d8822855e220fff082b255e7343f17
SS0809 <saurabh45215@gmail.com>
2022-11-05 20:55:07 +0530

    0.0.1

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
 echo exec('cd c:\xampp\htdocs\onlinecompiler'); 
 passthru('more abc.txt');
 ?>
+<html>
+    <head>
+       <title>Online Compiler</title> 
+       <link rel="stylesheet" href="style.css">
+       <script src="script.js"></script>
+    </head>
+    <body>
+        <h1>Online Compiler</h1>
+        <label>code</label>
+        <input type ="text">  
+        <input type ="file">  
+    </body>
+</html>
